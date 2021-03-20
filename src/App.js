dabd244cf01bf163716cceb5a73853b61fe1a190
Robert Koepp <rkoepp2@gmail.com>
2021-03-20 14:04:47 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Geolocation from "./Geolocation";
 
-function App() {
+const App = () => {
+ 
+const location = Geolocation()
+console.log(location)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+  <div className="Location">
+    latitude:{location.coordinates.lat}
+    longitude:{location.coordinates.lng}
+  </div>
+  </>
+  )
 }
 
 export default App;
